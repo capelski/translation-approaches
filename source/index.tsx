7419a529718d2ded795a597c8f91f249348d7b2c
Carles Capellas <capellas.carles@gmail.com>
2024-11-12 17:17:52 +0100

    Minor refactor

--- source/index.tsx
+++ source/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { OriginalApp } from './original-app';
+import { OriginalApp } from './1-original-app';
 
 const container = document.getElementById('app-placeholder')!;
 const root = createRoot(container);
 
-// 1. Original App
 root.render(<OriginalApp defaultLanguage="eng" />);
