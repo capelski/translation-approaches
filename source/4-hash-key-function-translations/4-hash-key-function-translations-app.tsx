769f2a31160178e4bf1a33bdad76013ebe7f19e9
Carles Capellas <capellas.carles@gmail.com>
2024-11-13 13:17:05 +0100

    Translate by using english text hash

---
+++ source/4-hash-key-function-translations/4-hash-key-function-translations-app.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { AppProps } from '../app-props';
+import { LanguageSelector } from '../language-selector';
+import { getHash } from './4-hash';
+import sources from './4-sources.json';
+
+const translate = (text: string, language: string) => {
+  const translations = sources[language as keyof typeof sources];
+  const translatedText = translations?.[getHash(text) as keyof typeof translations];
+  return translatedText || text;
+};
+
+export const HashKeyFunctionTranslationsApp: React.FC<AppProps> = (props: AppProps) => {
+  return (
+    <div>
+      <h1>{translate('Hello World', props.language)}</h1>
+      <p>
+        {translate(
+          'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+          props.language,
+        )}
+      </p>
+      <p>
+        {translate('Language selector', props.language)}:
+        <LanguageSelector {...props} />
+      </p>
+    </div>
+  );
+};
--- source/index.tsx
+++ source/index.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { OriginalApp } from './1-original-app/1-original-app';
 import { EnglishKeyTranslationsApp } from './2-english-key-translations/2-english-key-translations-app';
 import { EnumKeyTranslationsApp } from './3-enum-key-translations/3-enum-key-translations-app';
+import { HashKeyFunctionTranslationsApp } from './4-hash-key-function-translations/4-hash-key-function-translations-app';
 
 const container = document.getElementById('app-placeholder')!;
 const root = createRoot(container);
 
 const AppWrapper: React.FC = () => {
   const [language, setLanguage] = useState('eng');
+
+  return <HashKeyFunctionTranslationsApp language={language} setLanguage={setLanguage} />;
 
   return <EnumKeyTranslationsApp language={language} setLanguage={setLanguage} />;
 
